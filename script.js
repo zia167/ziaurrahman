130e2226a6a39e872ce381c23fd22571fb60eb37
Zia <ziaurrahman167@yahoo.com>
2023-05-04 13:10:16 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+$(document).ready(
+    $("#mailTo").click(function () {
+        const part2 = "kar";
+        const part1 = "aupendu";
+        const part3 = Math.pow(2, 6);
+        const part6 = ".ac.in";
+        const part5 = "iitkgp";
+
+        const x = confirm(
+            "Click \"OK\" to open your default email app."
+        );
+        if (x == true)
+            window.location.href = "mailto:" + part1 + part2 + String.fromCharCode(part3) + part5 + part6;
+
+
+    }),
+    $(".gScholar").mouseover(function () {
+        $(this).css("cursor", "pointer").css("color", "#527bbd").css("font-weight", "bold");
+    }),
+    $(".gScholar").mouseleave(function () {
+
+        $(this).css("color", "gray").css("font-weight", "normal").css("cursor", "context-menu");
+    }),
+    $(".webBadge").mouseover(function () {
+        $(this).removeClass('bg-dark').addClass('bg-secondary');
+    }),
+    $(".webBadge").mouseleave(function () {
+        $(this).removeClass('bg-secondary').addClass('bg-dark');
+    }), $(".pdfBadge").mouseover(function () {
+        $(this).removeClass('bg-info').addClass('bg-warning');
+    }),
+    $(".pdfBadge").mouseleave(function () {
+        $(this).removeClass('bg-warning').addClass('bg-info');
+    }),
+    $('.emailIcon').mouseover(function () {
+        $('.fa-envelope').removeClass('d-none');
+    }),
+    $('.emailIcon').mouseleave(function () {
+        $('.fa-envelope').addClass('d-none');
+    }),
+    $('.extLink').mouseover(function () {
+        $('.fa-external-link-alt').removeClass('d-none');
+    }),
+    $('.extLink').mouseleave(function () {
+        $('.fa-external-link-alt').addClass('d-none');
+    })
+);
